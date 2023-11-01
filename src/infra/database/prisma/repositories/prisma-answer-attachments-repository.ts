a3878001590cc41b36f6f37598ddd71d9d45bde7
Luan Vendt <luanvendt@gmail.com>
2023-11-01 14:22:12 -0300

    Implementando reposiórios attachments-comments

--- src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -1,16 +1,29 @@
 import { AnswerAttachmentRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
-import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { PrismaAnswerAttachmentMapper } from '../mappers/prisma-answer-attachment-mapper'
 
 @Injectable()
 export class PrismaAnswerAttachmentssRepository
-  implements AnswerAttachmentRepository
-{
-  findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]> {
-    throw new Error('Method not implemented.')
+  implements AnswerAttachmentRepository {
+  constructor(private prisma: PrismaService) { }
+
+  async findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]> {
+    const answerAttachments = await this.prisma.attachment.findMany({
+      where: {
+        answerId,
+      }
+    })
+
+    return answerAttachments.map(PrismaAnswerAttachmentMapper.toDomain)
   }
 
-  deleteManyByAnswerId(answerId: string): Promise<void> {
-    throw new Error('Method not implemented.')
+  async deleteManyByAnswerId(answerId: string): Promise<void> {
+    await this.prisma.attachment.deleteMany({
+      where: {
+        answerId,
+      }
+    })
   }
 }
